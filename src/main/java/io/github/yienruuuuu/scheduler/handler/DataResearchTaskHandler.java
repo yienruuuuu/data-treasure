54614f7005d93f81f129e83197967916e4827ab8
Eric Lee <c6b7yntz@gmail.com>
2026-05-21 21:01:30 +0800

    init

--- src/main/java/io/github/yienruuuuu/scheduler/handler/DataResearchTaskHandler.java
+++ src/main/java/io/github/yienruuuuu/scheduler/handler/DataResearchTaskHandler.java
@@ -1,4 +1,4 @@
-package io.github.yienruuuuu.scheduler.demo;
+package io.github.yienruuuuu.scheduler.handler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
